59b01381a09b51fa6a97396fc78dae0e4fe6fe4b
kenshiro.sugiyama <kenshi.0223@icloud.com>
2019-10-27 09:25:01 +0000

    基本機能の追加

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,58 @@
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.order(created_at: :desc)
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def new
+        @article=Article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def create
+        @article=Article.new(article_params)
+        
+         if @article.save
+            flash[:success] = '記事 が正常に投稿されました'
+            redirect_to @article
+         else
+            flash.now[:danger] = '記事 が投稿されませんでした'
+            render :new
+         end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(article_params)
+            flash[:success] = '記事 は正常に更新されました'
+            redirect_to @article
+        else
+            flash.now[:danger] = '記事 は更新されませんでした'
+            render :edit
+        end
+    end
+
+    def destroy
+         @article = Article.find(params[:id])
+          @article.destroy
+          
+        flash[:success] = '記事 は正常に削除されました'
+        redirect_to articles_url
+        
+    end
+    
+private
+
+def article_params
+ params.require(:article).permit(:title ,:body)   
+end
+
+
+end
